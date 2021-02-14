a3ff6d535e6c7f49968dfc16080cfad7cc4d00bd
ahojulius <ahojulius@gmail.com>
2021-02-14 18:23:20 +0200

    add plugins, setup them, define jwt

--- wp-content/mu-plugins/wonsta-start/start.php
+++ wp-content/mu-plugins/wonsta-start/start.php
@@ -23,6 +23,24 @@ function init_wonsta(){
     if(getenv('WORDPRESS_NAME')){
         update_option( 'blogname', getenv('WORDPRESS_NAME') );
     }
+
+    if(getenv('WORDPRESS_TEMPLATE')){
+        if(wp_get_theme(getenv('WORDPRESS_TEMPLATE'))){
+            switch_theme(getenv('WORDPRESS_TEMPLATE'));
+        }
+    }
+    
+    // Setup plugins
+    $plugins = array('jwt-auth', 'elementor');
+
+    foreach($plugins as $slug){
+        if(!is_plugin_active($slug)){
+            activate_plugin($slug);
+        }
+    }
+    
+    define('JWT_AUTH_SECRET_KEY', 'your-top-secret-key');
+    
 }
 
 add_action('init', 'init_wonsta', 20); 
