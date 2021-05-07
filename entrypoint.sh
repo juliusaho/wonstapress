29373ee37262fcd5af4c1caca22db370007646f0
juliusaho <ahojulius@gmail.com>
2021-05-07 14:38:30 +0300

    change owner to be testuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,6 +40,6 @@ wp --path=/usr/src/wordpress core update
 wp --path=/usr/src/wordpress core update-db
 
 # Setup correct ownership
-chown -R nginx.nginx /var/www/wp-content
+chown -R testuser.testuser /var/www/wp-content
 
 exec "$@"
