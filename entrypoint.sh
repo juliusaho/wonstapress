f98a7509739c851ccbbddd8d6efa971fa183afec
juliusaho <ahojulius@gmail.com>
2021-05-07 10:34:58 +0300

    update entrypoint copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,16 @@ set -e
 echo 'About to change directories'
 
 # Remove lost+found directory
-if [ -d /home/testuser/lost+found ]; then
+if [ -d /var/www/wp-content/lost+found ]; then
     echo 'Removing dir'
-    rmdir /home/testuser/lost+found 2>/dev/null
+    rmdir /var/www/wp-content/lost+found 2>/dev/null
 fi
 
 # Check if volume is empty
 if [ ! "$(ls -A "/var/www/wp-content" 2>/dev/null)" ]; then
     echo 'Setting up wp-content volume'
     # Copy wp-content from Wordpress src to volume
-    cp -r /usr/src/wordpress/wp-content/ /var/www/wp-content/.
+    cp -a /usr/src/wordpress/wp-content/. /var/www/wp-content/
     chown -R nobody.nobody /var/www
 
     # Generate secrets
