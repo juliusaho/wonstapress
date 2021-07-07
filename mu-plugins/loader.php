258e90e20d664336455257e6f684d12b9b21562f
juliusaho <ahojulius@gmail.com>
2021-07-07 14:49:57 +0300

    Add autoptimize

--- mu-plugins/loader.php
+++ mu-plugins/loader.php
@@ -1,7 +1,15 @@
 <?php
+/** 
+* Plugin Name: Wonsta MU-Loader
+* Description: Must use plugins to use Wonsta 
+* Version:     1.0
+* Text Domain: wonsta_loader
+* Author:      Wonsta
+* Author URI:  https://wonsta.io
+* License:     MIT
+* License URI: https://opensource.org/licenses/MIT
+*/
 require WPMU_PLUGIN_DIR.'/wonsta-worker/wonsta-worker.php';
 require WPMU_PLUGIN_DIR.'/redis-cache/redis-cache.php';
-
 require WPMU_PLUGIN_DIR.'/wp-password-bcrypt/wp-password-bcrypt.php';
-
-require WPMU_PLUGIN_DIR.'/cache-enabler/cache-enabler.php';
+require WPMU_PLUGIN_DIR.'/autoptimize/autoptimize.php';
