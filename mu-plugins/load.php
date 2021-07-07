e97de062896c0550cec6052183a16a91cbbaf5ec
juliusaho <ahojulius@gmail.com>
2021-07-07 11:20:16 +0300

    fix load

--- mu-plugins/load.php
+++ mu-plugins/load.php
@@ -2,4 +2,5 @@
 require WPMU_PLUGIN_DIR.'/wonsta-worker/wonsta-worker.php';
 require WPMU_PLUGIN_DIR.'/redis-cache/redis-cache.php';
 require WPMU_PLUGIN_DIR.'/wp-password-bcrypt/wp-password-bcrypt.php';
+
 require WPMU_PLUGIN_DIR.'/cache-enabler/cache-enabler.php';
